e0d59b4917462e18fb917feea7537b35c05e1ae7
Ronin Carrion <rdcarrion3@utpl.edu.ec>
2021-10-22 10:51:05 -0500

    primer commit

---
+++ proyecto001/src/proyecto001/Proyecto001.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto001;
+
+/**
+ *
+ * @author ronni
+ */
+public class Proyecto001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Generando un miniprogrma en Java");
+        System.out.println("Usando Netbeans");
+        System.out.println("junto");
+        System.out.println("Git y GitHub");
+    
+    }
+    
+}
